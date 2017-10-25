050c8eed412134d293e1c0cedb0a8161db571dca
Dennis Vriend <dnvriend@gmail.com>
2017-10-25 17:10:59 +0200

    minor

--- 01-hello-scala/src/main/scala/hello/ErrorHandler.scala
+++ 01-hello-scala/src/main/scala/hello/ErrorHandler.scala
@@ -3,7 +3,7 @@ package hello
 import com.amazonaws.services.lambda.runtime.Context
 import scalaz.Scalaz._
 
-object ErrorHandler extends LambdaRequestHandler[HelloMessage] {
+object ErrorHandler extends LambdaRequestHandler[String] {
   override def handle(request: Request, ctx: Context): Response = {
     Option.empty[String].toSuccessNel("Error Handler")
   }
